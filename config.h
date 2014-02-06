5effb9944e3f7d4add1257204d86fd6cb3214967
sin <sin@2f30.org>
2014-02-06 11:34:38 +0000

    Remove config.h from repo

--- config.h
+++
@@ -1,6 +0,0 @@
-/* See LICENSE file for copyright and license details. */
-static const char *fifopath = "/var/run/sinit.fifo";
-static Command commands[] = {
-	{ "poweroff",   cmdpoweroff,   {0} },
-	{ "reboot",     cmdreboot,     {0} },
-};
