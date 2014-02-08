fa62232dceae65c3d07a980653b42ecc997ff753
sin <sin@2f30.org>
2014-02-08 13:01:55 +0000

    Remove unused #includes

--- sinit.c
+++ sinit.c
@@ -1,13 +1,10 @@
 /* See LICENSE file for copyright and license details. */
 
 #include <errno.h>
-#include <fcntl.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/signalfd.h>
-#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
